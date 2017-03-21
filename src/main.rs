fea01d5db25ee771bd87e70a61f284cac369a1c3
Jon0 <Jono4728@gmail.com>
2017-03-21 23:42:21 +1300

    open device

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,21 @@ extern crate byteorder;
 
 mod drm;
 
+use std::ffi::CString;
+use libc::*;
 use drm::ffi::xf86_drm::*;
 
 
+fn open_device(name: &str) {
+    let devname = CString::new(name).unwrap();
+    unsafe {
+        let fd = open(devname.as_ptr(), O_RDWR);
+    }
+}
+
+
 fn main() {
+
     let devices = [drmDevice::default()];
     let devices_ptr = devices.as_ptr() as *mut drmDevice;
     unsafe {
